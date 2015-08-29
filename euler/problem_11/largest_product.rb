1dd7309762413b08542f8b903ee63993b6f93377
Dana Scheider <dana.scheider@gmail.com>
2015-08-29 09:29:31 -0700

    Add features to the multiplier class

--- euler/problem_11/largest_product.rb
+++ euler/problem_11/largest_product.rb
@@ -3,7 +3,60 @@ class Multiplier
     @grid = grid
   end
 
+  def diagonal row_index, col_index
+    diagonal = nil
+
+    if row_index < @grid.length - 1 && col_index < @grid[0].length - 1
+      diagonal = @grid[row_index + 1][col_index + 1]
+    end
+
+    diagonal
+  end
+
+  def largest_col_product
+    product = 1
+
+    for i in 1..@grid[0].length - 1
+      col = @grid.map {|n| n[0] }
+      col_product = col.inject(1, &:*)
+      product = col_product if col_product > product
+    end
+
+    product
+  end
+
+  def largest_row_product
+    product = 1
+
+    @grid.each do |row|
+      row_product = row.inject(1, &:*)
+      product = row_product if row_product > product 
+    end
+
+    product
+  end
+
   def get_largest_product
-    @grid.flatten.sort
+    product = [largest_row_product, largest_col_product].max
+
+    @grid.each do |row|
+      row_index = @grid.index row
+
+      row.each do |val|
+        col_index = row.index val
+        to_multiply = [val]
+
+        to_multiply << diagonal(row_index, col_index)
+        to_multiply << diagonal(row_index + 1, col_index + 1)
+        to_multiply << diagonal(row_index + 2, col_index + 2)
+
+        if to_multiply.compact.length == 4
+          sum = to_multiply.inject(1, &:*)
+          product = sum if sum > product
+        end
+      end
+    end
+
+    product
   end
 end
--- euler/problem_11/spec/largest_product_spec.rb
+++ euler/problem_11/spec/largest_product_spec.rb
@@ -2,17 +2,46 @@ require 'spec_helper'
 
 describe Multiplier do 
   let(:multiplier) { Multiplier.new(grid) }
-  let(:product) { 66_670_331 }
-  let(:grid) {
-    [[73, 15, 97, 92],
-     [97, 39, 91, 61],
-     [10, 67, 91, 46],
-     [8, 84, 83, 74]]
-  }
+  let(:product) { 20_999_433 }
 
   describe 'get_largest_product method' do 
-    it 'returns the correct sum' do 
-      expect(multiplier.get_largest_product).to equal product
+    context 'largest product is on a straight row' do 
+      let(:grid) {
+        [[73, 15, 97, 92],
+         [97, 39, 91, 61],
+         [10, 67, 91, 46],
+         [8, 84, 83, 74]]
+      }
+
+      it 'returns the correct product' do 
+        expect(multiplier.get_largest_product).to eql product
+      end
+    end
+
+    context 'largest product is ona column' do 
+      let(:grid) {
+        [[99, 47, 61, 33],
+         [99, 20, 82, 40],
+         [99, 18, 81, 13],
+         [99, 12, 90, 66]]
+      }
+
+      it 'returns the correct product' do 
+        expect(multiplier.get_largest_product).to eql 99 ** 4
+      end
+    end
+
+    context 'largest product is on the diagonal' do 
+      let(:grid) {
+        [[99, 47, 61, 33],
+         [20, 99, 82, 40],
+         [18, 81, 99, 13],
+         [12, 90, 66, 99]]
+      }
+
+      it 'returns the correct product' do 
+        expect(multiplier.get_largest_product).to eql 99 ** 4
+      end
     end
   end
 end
