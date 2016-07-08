1de2f59f85a165c1b7e224869b8ad0b2b5345e18
Dana Scheider <dana.scheider@gmail.com>
2016-07-08 11:15:50 -0700

    Second passing RSpec

--- interview_cake/ex1/lib/profit_calculator.rb
+++ interview_cake/ex1/lib/profit_calculator.rb
@@ -1,5 +1,5 @@
 module ProfitCalculator
   def self.get_max_profit(stock_prices)
-    1
+    stock_prices.last - stock_prices.first
   end
 end
--- interview_cake/ex1/spec/profit_calculator_spec.rb
+++ interview_cake/ex1/spec/profit_calculator_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe ProfitCalculator do
       expect(described_class.get_max_profit(stock_prices)).to eq profit
     end
   end
+
+  context "larger, strictly increasing array" do 
+    let(:stock_prices) { [ 1, 2, 3, 4, 5 ] }
+    let(:profit)       { 4 }
+
+    it "returns the highest profit" do 
+      expect(described_class.get_max_profit(stock_prices)).to eq profit
+    end
+  end
 end
