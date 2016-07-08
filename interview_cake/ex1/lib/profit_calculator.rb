8a2a38178e605aaa93cbf24ebd18d2a75c0c1b50
Dana Scheider <dana.scheider@gmail.com>
2016-07-08 13:01:50 -0700

    Finish example 1

--- interview_cake/ex1/lib/profit_calculator.rb
+++ interview_cake/ex1/lib/profit_calculator.rb
@@ -1,5 +1,9 @@
 module ProfitCalculator
   def self.get_max_profit(stock_prices)
-    stock_prices.last - stock_prices.first
+    buy_price  = stock_prices[0..-2].min
+    buy_index  = stock_prices.find_index(buy_price)
+    sell_price = stock_prices[buy_index + 1..-1].max
+
+    sell_price - buy_price
   end
 end
--- interview_cake/ex1/spec/profit_calculator_spec.rb
+++ interview_cake/ex1/spec/profit_calculator_spec.rb
@@ -16,4 +16,31 @@ RSpec.describe ProfitCalculator do
       expect(described_class.get_max_profit(stock_prices)).to eq profit
     end
   end
+
+  context "small, strictly decreasing array" do 
+    let(:stock_prices) { [ 2, 1 ] }
+    let(:profit)       { -1 }
+
+    it "returns the highest profit" do 
+      expect(described_class.get_max_profit(stock_prices)).to eq profit
+    end
+  end
+
+  context "larger, strictly decreasing array" do 
+    let(:stock_prices) { [ 10, 8, 3, 2, 1 ] }
+    let(:profit)       { -1 }
+
+    it "returns the highest profit" do
+      expect(described_class.get_max_profit(stock_prices)).to eq profit
+    end
+  end
+
+  context "non-monotonic array" do 
+    let(:stock_prices) { [ 6, 8, 1, 6, 2 ] }
+    let(:profit)       { 5 }
+
+    it "returns the highest profit" do 
+      expect(described_class.get_max_profit(stock_prices)).to eq profit
+    end
+  end
 end
